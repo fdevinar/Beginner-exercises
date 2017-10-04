900362e28d60ef4e29cd31efbaea4bf7b3681e6b
Fabrício Devinar <fc_devinar@hotmail.com>
2017-10-04 16:59:25 -0300

    Add files via upload

--- check_even.py
+++ check_even.py
@@ -1,9 +1,14 @@
 num = input()
 list(num)
+even = 0
+uneven = 0
 
 for n in num:
     if (int(n)%2 != 1):
         print ('Par/Even')
+        even += 1
     else:
         print ('Impar/Uneven')
-    
+        uneven += 1
+
+print ('Even: ' + str(even) + ' - Uneven: ' + str(uneven))
