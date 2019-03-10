25fa1d264992658e3ab121782b4d9029ce26bbeb
Fabrício Devinar <fc_devinar@hotmail.com>
2019-03-10 19:19:28 -0300

    Add files via upload

--- time_converter.py
+++ time_converter.py
@@ -1,18 +1,28 @@
 def time_converter (time):
-    print ('Tamanho: ' + str(len(time)))
-    if (int(time[0:2:1]) >= 12):
-        print ('PM')
+
+    if (len(time) == 5):
+        hour = time[0:2:1]
+        minute = time[3:5:1]
     else:
-        print ('AM')
+        hour = time[0:1:1]
+        minute = time[2:4:1]
 
-    
-    print (time[0:2:1])
-    print (time[3:5:1])
+    if (int(hour) == 0 and int(minute) == 0):
+        return ('12:00 a.m.')  
 
+    if (int(hour[0:1:1]) == 0):
+        hour = hour[1:2:1]
 
+    if (int(hour) == 12):
+        return (str(hour) + ':' + str(minute) + ' p.m.')
+        
+    if (int(hour) > 12):
+        hour = int(hour) - 12
+        return (str(hour) + ':' + str(minute) + ' p.m.')
+    else:
+        return (str(hour) + ':' + str(minute) + ' a.m.')
 
-
-
-time_converter('23:15')
+        
+print(time_converter('00:00'))
 
 
