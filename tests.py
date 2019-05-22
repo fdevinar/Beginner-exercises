88a8ca904e016f863eabee5f72e290ba0e3a04ca
Fabrício Devinar <fc_devinar@hotmail.com>
2019-05-22 19:23:05 -0300

    Add files via upload

--- tests.py
+++ tests.py
@@ -1,22 +1,27 @@
 # https://py.checkio.org/en/mission/popular-words/
 
 def popular_words (text, array):
-    arrtext = text.split()
 
+    text = text.lower()
+    text = text.split()
+
+    print (text)
+    
     popudict = {}
 
-    for word in arrtext:
-        if word in array:
-            if word in popudict.keys():
-                
-                print ('Loopou')
-                
-            popudict.update({word: 1})
+    for word in array:
+        if word in text:
+            if (word in popudict.keys()):
+                popudict.update({word: (popudict.get(word)+1)})
+            else:
+                popudict.update({word: 1})
+        else:
+            popudict.update({word: 0})
+            
+    return (popudict)    
 
-    print (popudict.keys())
-    print (popudict.values())
+#logica incorreta - nao deveria loopar por array
+print (popular_words('a b c',['a','b','b','b','c','d']))
 
+print (popular_words('When I was One I had just begun When I was Two I was nearly new',['i','was','three','near']))
 
-
-
-popular_words('a b c',['a','b','b'])
