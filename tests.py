a15c9787d3ed3c8d52ae700357709ba72a74a899
Fabrício Devinar <fc_devinar@hotmail.com>
2019-05-25 15:06:00 -0300

    Add files via upload

--- tests.py
+++ tests.py
@@ -1,102 +1,31 @@
-#(a) 3+4+5+6+7+8+9, (b) 9+10+11+12, (c) 13+14+15 and (d) 42.
-
-consecutive = 0
-num = 42
-count = 0
-numlist = []
-soma = 0
-
-for x in range(1, num+1):
-    numlist.append(x)
-
-print (numlist)
-
-#print(sum(numlist))
-
 '''
-print (sum(numlist[count:count]))
-print (sum(numlist[count:count+1]))
-print (sum(numlist[count:count+2]))
-print (sum(numlist[count:count+3]))
-print (sum(numlist[count:count+4]))
+all_the_same([1, 1, 1]) == True
+
+all_the_same([1, 2, 1]) == False
+
+all_the_same(['a', 'a', 'a']) == True
+
+all_the_same([]) == True
 '''
 
+def all_the_same (lista):
+    if not lista:
+        return True
 
-x = 0
-
-
-while (soma < num):
-    print ('**********************')
-    print ('Count:')
-    print (count)
-    print ('Soma:')
-    print (sum(numlist[x:count]))
-    print (numlist[x:count])
-    soma = (sum(numlist[x:count]))
-    if (soma == num):
-        consecutive += 1
-    print('Nova soma:')
-    print (soma)
-    count += 1
-
-#print ('Resultado final:')
-#print (consecutive)
-
-'''
+    comp = lista[0]
+    equal = False
 
 
-print (numlist[0:0])
-print (sum((numlist[0:0]))) = 0
-
-print (numlist[0:1])
-print (sum((numlist[0:1]))) = 1
-
-
-print (numlist[0:2])
-print (sum((numlist[0:2]))) = 3
-
-
-print (numlist[0:3])
-print (sum((numlist[0:3]))) = 6
-
-print (numlist[0:4])
-print (sum((numlist[0:4]))) = 10
-
-print (numlist[0:5])
-print (sum((numlist[0:5]))) = 15
-'''
-
-'''
-print ('Next loop')
-
-print (numlist[0+1:0+1])
-print (sum((numlist[0+1:0+1])))
-
-print (numlist[0+1:1+1])
-print (sum((numlist[0+1:1+1])))
+    
+    for element in lista:
+        if (element == comp):
+            equal = True
+        else:
+            return False
+    if (equal):
+        return True
 
 
-print (numlist[0+1:2+1])
-print (sum((numlist[0+1:2+1])))
-
-
-print (numlist[0+1:3+1])
-print (sum((numlist[0+1:3+1])))
-
-print (numlist[0+1:4+1])
-print (sum((numlist[0+1:4+1])))
-
-print (numlist[0+1:5+1])
-print (sum((numlist[0+1:5+1])))
-
-print ('Next loop')
-'''
-
-
-
-
-
-
-
-
-
+print (all_the_same([]))
+        
+        
