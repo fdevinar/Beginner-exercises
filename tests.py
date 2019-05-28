697c439bdb3bae5cd2b9353fb4cd931787400e17
Fabrício Devinar <fc_devinar@hotmail.com>
2019-05-28 12:25:47 -0300

    Add files via upload

--- tests.py
+++ tests.py
@@ -1,16 +1,25 @@
-#https://py.checkio.org/en/mission/non-unique-elements/
+''' https://py.checkio.org/en/mission/sun-angle/
+sun_angle("07:00") == 15
+sun_angle("12:15"] == 93.75
+sun_angle("01:23") == "I don't see the sun!"
+'''
 
-def checkio(array):
-    non_unique_list = []
-    for element in array:
-        if (array.count(element) > 1):
-            non_unique_list.append(element)
+def sun_angle(time):
+    hour = time[1:2]
+    minute = time[3:5]
 
-    return (non_unique_list)
-            
+    time = int(hour) * 60 + int(minute)
+
+    #print (hour ,minute)
+
+    #print (time)
+
+    # tratar horario noturno
+    # 2 casas decimais
+    
+    angle = (time - 360 / 4.00)
+
+    return angle
 
 
-#checkio(['1','2','1','2','3'])
-
-print (checkio(['5','5','5','5','5']))
-
+print(sun_angle('12:15'))
